486a2950890d28d5bb3896eed1c660f7b466f47f
Lucy <lucynn11@gmail.com>
2018-01-20 00:24:02 +0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA7G4uT-hqZ-hftan2bPXct3UU8mwBKns4",
+    authDomain: "react-chat-room-5d397.firebaseapp.com",
+    databaseURL: "https://react-chat-room-5d397.firebaseio.com",
+    projectId: "react-chat-room-5d397",
+    storageBucket: "react-chat-room-5d397.appspot.com",
+    messagingSenderId: "797104072210"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -13,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+         <div id="roomslist">{this.props.rooms}</div>
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,14 +1,31 @@
-this.state = {
-  rooms: []
-};
+import React, { Component } from 'react';
 
-   this.rooms  this.props.firebase.database().ref('rooms');
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        rooms: []
+    }
 
-componentDidMount(){
-  this.roomsRef.on('child_added', snapshot =>{
-    const room = snapshot.val();
-    room.key = snapshot.key;
-    console.log(snapshot);
-    this.setState({ rooms: this.state.rooms.concat( room ) })
-  });
+   this.roomsRef = this.props.firebase.database().ref('rooms');
 }
+
+   componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+       console.log(snapshot);
+       const room = snapshot.val();
+       room.key = snapshot.key;
+       this.setState({ rooms: this.state.rooms.concat( room ) })
+     });
+}
+
+    render(){
+      return(
+        <div className="RoomList">{this.state.rooms.map()}</div>
+      );
+    }
+}
+
+
+
+export default RoomList;
