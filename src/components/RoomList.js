11017f050b8e91697e4510872b2d5cbf94c4dae6
Lucy <lucynn11@gmail.com>
2018-01-20 13:41:06 +0400

    cp3-WIP

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-        rooms: []
+        rooms: [],
+        newRoomName:' ',
+        submit:
     }
 
    this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -17,12 +20,38 @@ class RoomList extends Component {
        room.key = snapshot.key;
        this.setState({ rooms: this.state.rooms.concat( room ) })
      });
+
+     handleNewRoomName(e){
+         console.log("new room name:"+ e.target.value)
+         this.setState({newRoomName: e.target.value})
+     }
+
+     createRoom(e){
+       this.roomsRef.push({
+         name: newRoomName
+       });
+
+     }
 }
 
     render(){
       const room = this.state.rooms.map(room);
           return(
-             <li>{this.state.rooms}</li>
+          <div>
+            <li>
+                 {room}
+            </li>
+            <form name="formname">
+                <input type="text"
+                name="fname"
+                placeholder="New Room Name"
+                onChange={this.state.newRoomName}
+                handleNewRoomName={(e) => this.handleNewRoomName(e)}>
+                <br />
+                <button onClick={this.createRoom}>Create Room</button>
+
+             </form>
+          </div>
 
       );
     }
